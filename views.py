1a45af750c29dee7750136aefdc28e2b0534fbf6
Artem <66700598+artemkiryu@users.noreply.github.com>
2022-10-03 20:23:09 -0400

    Add files via upload

--- views.py
+++ views.py
@@ -2,4 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse ('Hellow World')
+    return HttpResponse('Hellow World')
+
+
+def new (request):
+    return HttpResponse('New Products')
+
+
+
